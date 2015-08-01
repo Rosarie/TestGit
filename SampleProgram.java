1fe608927c14f2dcb2fc88a38a18d81503854ea7
Rosarie <rosarie.gillera@wvsu.edu.ph>
2015-08-01 10:08:37 +0800

    Create SampleProgram.java

---
+++ SampleProgram.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import javax.swing.*;
+
+public class SampleProgram{
+  public static void main(String[] args){
+    Scanner dataIn=new Scanner(System.in);
+    String name=JOptionPane.showInputDialog("Enter name:");
+    System.out.println("Enter age:");
+     int age=dataIn.nextInt();
+     }
+     }
+    
